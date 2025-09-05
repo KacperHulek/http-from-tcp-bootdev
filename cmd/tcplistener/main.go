1fd75fe5b8cd2ac95669479952048bec1a765756
Kacper <kacper2208@gmail.com>
2025-09-05 06:11:35 +0200

    finish ex

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,49 +1,11 @@
 package main
 
 import (
-	"bytes"
+	"firstgoproject/internal/request"
 	"fmt"
-	"io"
 	"log"
 	"net"
 )
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	out := make(chan string, 1)
-
-	go func() {
-		defer f.Close()
-		defer close(out)
-
-		str := ""
-
-		for {
-			data := make([]byte, 8)
-			n, err := f.Read(data)
-
-			if err != nil {
-				break
-			}
-
-			data = data[:n]
-			if i := bytes.IndexByte(data, '\n'); i != -1 {
-				str += string(data[:i])
-				data = data[i+1:]
-				out <- str
-				str = ""
-			}
-			str += string(data)
-
-		}
-
-		if len(str) != 0 {
-			out <- str
-		}
-
-	}()
-
-	return out
-}
 
 func main() {
 
@@ -57,9 +19,14 @@ func main() {
 		if err != nil {
 			log.Fatal("error", "error", err)
 		}
-		for line := range getLinesChannel(conn) {
 
-			fmt.Printf("read: %s\n", line)
+		r, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatal("error", "error", err)
 		}
+		fmt.Printf("Request line:\n")
+		fmt.Printf("- Method: %s\n", r.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", r.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", r.RequestLine.HttpVersion)
 	}
 }
